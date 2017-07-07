f7d243d6a7246365167664c47d24996af0c778a6
vladimirosa <vladimiro.sa@hitachiconsulting.com>
2017-07-07 09:41:06 +0100

    Unfinished work for UI

---
+++ uiSelenium/uiSelenium/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testFrameworks.uiSelenium
+{
+    public static void main(String[] args) {
+
+		// Declaring and initialising the HtmlUnitWebDriver
+		HtmlUnitDriver unitDriver = new HtmlUnitDriver();
+
+		// open google.com webpage
+		unitDriver.get("http://google.com");
+
+		System.out.println("Title of the page is -> " + unitDriver.getTitle());
+
+		// find the search edit box on the google page
+		WebElement searchBox = unitDriver.findElement(By.name("q"));
+
+		// type in Selenium
+		searchBox.sendKeys("Selenium");
+
+		// find the search button
+		WebElement button = unitDriver.findElement(By.name("gbqfba"));
+
+		// Click the button
+		button.click();
+
+		System.out.println("Title of the page is -> " + unitDriver.getTitle());
+
+	}
+}
